c95cd885880a28be592e1d24bfbf1ac653d016a5
lalkab1309 <nikitos0770654@mail.ru>
2024-05-16 10:24:10 +0300

    Обновлены стили
    Добавлены кнопки удаления (требуют исправления)

--- protocols/client/protocols/src/components/main/SlidesCreator.jsx
+++ protocols/client/protocols/src/components/main/SlidesCreator.jsx
@@ -3,6 +3,13 @@ import TextInput from './TextInput';
 import styles from '../../styles/slidesCreator.module.css'
 
 const SlidesForm = () => {
+  const [titleValue, setTitleValue] = useState('');
+  const [textValue, setTextValue] = useState('');
+  
+  const handleTitleChange = (value) => {
+    setTitleValue(value);
+  };
+
   const [slides, setSlides] = useState([]);
   const [newSlideText, setNewSlideText] = useState('');
 
@@ -10,6 +17,11 @@ const SlidesForm = () => {
     setSlides([...slides, { text: newSlideText }]);
     setNewSlideText('');
   };
+  {/*const handleAddSlide = () => {
+    const newSlide = { text: newSlideText, id: uuidv4() };
+    setSlides([...slides, newSlide]);
+    setNewSlideText('');
+  };*/}
 
   const handleTextChange = (index, text) => {
     setSlides(slides.map((slide, i) => {
@@ -20,19 +32,27 @@ const SlidesForm = () => {
     }));
   };
 
+  const handleDeleteSlide = (index) => {
+    return () => {
+      setSlides(slides.filter((slide, i) => i !== index));
+    };
+  };
+
   return (
     <div className={styles.main}>
-      {slides.map((slide, index) => (
-        <div key={index}>
+      {/*slides.map((slide)*/slides.map((slide, index) => (
+        <div /*key={slide.id}*/key={index}>
           <TextInput
             type="text"
             value={slide.text}
             onChange={(e) => handleTextChange(index, e.target.value)}
           />
+           <button onClick={handleDeleteSlide(index)} className={styles.slidesCreatorButtonDelete}>Удалить слайд</button>
         </div>
       ))}
       
       <button onClick={handleAddSlide} className={styles.slidesCreatorButton}>Добавить слайд</button>
+      <button onClick={handleTitleChange} disabled={!titleValue || !textValue} className={styles.slidesCreatorButton}>Отправить</button>
     </div>
   );
 };
