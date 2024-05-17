f4c33c0ed408e4a93fd7b5afdc25ec7c52b5d733
chugumba <chugumba@rambler.ru>
2024-05-17 13:58:41 +0300

    Завершена серверная часть приложения

--- protocols/server/router.js
+++ protocols/server/router.js
@@ -1,11 +1,45 @@
 const Router = require ('express');
 const router = new Router()
-const controller = require ();
+const mysql = require('mysql2');
 
-router.post('/protocol', {
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: '12345',
+    database: 'protocols'
+  });
+  router.post('/', async (req, res) => {
+    const titleValue = req.body.title;
+    const dataToSend = req.body.dataToSend;
+    let insertedId = '';
+  
+    const query = `INSERT INTO protocol (title) VALUES (?)`;
+    db.query(query, [titleValue], (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send({ message: 'Error adding protocol' });
+      } else {
+        insertedId = results.insertId;
+        console.log(`Data saved to database: ${titleValue} with id ${insertedId}`);
+  
+        let counter = 0;
+        dataToSend.forEach((slide, index) => {
+            const slideQuery = 'INSERT INTO slides (presentation_id, `order`, content) VALUES (?, ?, ?)';
+            db.query(slideQuery, [insertedId, index, slide], (err, results) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(`Slide saved to database: ${slide} with order ${index}`);
+              counter++;
+              if (counter === dataToSend.length) {
+                res.send({ message: 'Protocol and slides saved successfully' });
+              }
+            }
+          });
+        });
+      }
+    });
+  });
+  
 
-    async addProtocol (req,res) {
-        
-    }
-
-})
+module.exports = router;
--- protocols/server/server.js
+++ protocols/server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const route = require('./router')
 
+const corsOptions = {
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'] 
+};
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
+
+app.use('/protocol', route)
 
 const PORT = 5000;
 
